257cd5019951dc4d456d53741b52fd1a70735089
Илья Волченков <zvolkzrus@gmail.com>
2015-11-05 11:13:16 +0300

    Init commit

---
+++ OSLab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Школьники!");
+            Console.ReadKey();
+        }
+    }
+}
