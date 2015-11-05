7cd21178eb10771bce0bc1ca61d1ccaf54d7d837
Илья Волченков <zvolkzrus@gmail.com>
2015-11-05 11:30:04 +0300

    +Task

---
+++ OSLab1/Task.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSLab1
+{
+    enum Priority
+    {
+        Lazy,
+        Low,
+        Normal,
+        High,
+        Critical
+    };
+
+    class Task
+    {
+        string _name;
+        int _pid;
+        Priority _priority;
+        TimeSpan workTime = new TimeSpan(0);
+
+        Task(string name, Priority priority, int timeToSleep)
+        {
+            _name = name;
+            _priority = priority;
+        }
+    }
+}
